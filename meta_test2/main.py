369695bf30380e7a709c9d6e362634f2289d56af
Nikita Nagin <n94o@yandex.ru>
2022-10-05 00:17:48 +0300

    class Base

--- meta_test2/main.py
+++ meta_test2/main.py
@@ -1,64 +1,9 @@
-# import threading
-# threading._DummyThread._Thread__stop = lambda x: 42
 import json
-
-
-class customtype(type):  ## Определить новый метакласс
-    def __init__(cls, cls_name, bases, attrs):
-        temp_list = []
-        if isinstance(attrs, dict):
-            for k, v in attrs.items():
-                if isinstance(v, dict):
-                    attrs[k] = customtype('', (customtype, ), v)()
-                    break
-                elif isinstance(v, list):
-                    for attr in v:
-                        if isinstance(attr, dict):
-                            for k, v in attr.items():
-                                if isinstance(v, dict):
-                                    attrs[k] = customtype('', (), v)()
-                                    break
-                                else:
-                                    temp_list.append(attr[k])
-                                    attrs[k] = temp_list
-                        else:
-                            temp_list.append(attr)
-                            attrs[k] = temp_list
-
-
-# class Base(dict):
-#     def __init__(self, attrs):
-#         if isinstance(attrs, dict):
-#             for key, value in attrs.items():
-#                 if isinstance(value, dict):
-#                     setattr(self, key, Base(value))
-#                 else:
-#                     setattr(self, key, value)
-
 
 class Base(dict):
     def __init__(self, attrs):
-        if isinstance(attrs, dict):
-            for key, value in attrs.items():
-                if isinstance(value, dict):
-                    setattr(self, key, Base(value))
-                else:
-                    setattr(self, key, value)
-    # def __init__(self, attrs):
-    #     temp = []
-    #     if isinstance(attrs, dict):
-    #         for key, value in attrs.items():
-    #             if isinstance(value, dict):
-    #                 setattr(self, key, Base(value))
-    #             if isinstance(value, list):
-    #                 for val in value:
-    #                     if isinstance(val, dict):
-    #                         setattr(self, key, val)
-    #                     else:
-    #                         setattr(self, key, value)
-    #             else:
-    #                 setattr(self, key, value)
-
+        pass
+                    # vsegda peredau key:value krome str
     def __getattr__(self, attr):
         return self.get(attr)
 
@@ -82,54 +27,78 @@ class Base(dict):
     def type_coerce_attrs(self):
         pass
 
-# class SimpleBase:
-#     def __init__(self, attrs):
-#         if attrs:
-#             setattr(self, key, SimpleBase(attrs))
+def type_coerce(obj, objType):
+    if isinstance(obj, objType):
+        ret = BaseChild(obj)
+    else:
+        ret = obj
+    return ret
+
+def type_coerce_list(obj, objType):
+    if isinstance(obj, list):
+        ret = []
+        for entry in obj:
+            ret.append(type_coerce(entry, dict))
+    else:
+        ret = None
+    return ret
+class BaseChild(Base):
+    def __init__(self, attrs):
+        if attrs is None:
+            attrs = {}
+
+        Base.__init__(self, attrs)
+
+        if isinstance(attrs, dict):
+            for key, value in attrs.items():
+                key.replace('.', '_')
+                if isinstance(value, str):
+                    setattr(self, key, value)
+                elif isinstance(value, dict):
+                    setattr(self, key, BaseChild(value))
+                elif isinstance(value, list):
+                    setattr(self, key, type_coerce_list(value, BaseChild))
 
 
 if __name__ == '__main__':
-    ambari_json = {
-  "items" : [
-    {
-      "href" : "http://localhost:8080/api/v1/clusters/cc/configurations/service_config_versions?service_name=HDFS&service_config_version=2",
-      "configurations" : [
-        {
-          "Config" : {
-            "cluster_name" : "HDP",
-            "stack_id" : "HDP-2.2"
-          },
-          "type" : "core-site",
-          "tag" : "version2",
-          "version" : 2,
-          "properties" : {
-                "security.inter.datanode.protocol.acl" : "*",
-                "security.refresh.usertogroups.mappings.protocol.acl" : "hadoop",
-                "security.client.datanode.protocol.acl" : "*",
-                "security.admin.operations.protocol.acl" : "hadoop",
-                "security.inter.tracker.protocol.acl" : "*",
-                "security.datanode.protocol.acl" : "*",
-                "security.job.client.protocol.acl" : "*",
-                "security.client.protocol.acl" : "*",
-                "security.job.task.protocol.acl" : "*",
-                "security.refresh.policy.protocol.acl" : "hadoop",
-                "security.namenode.protocol.acl" : "*"
-          },
-          "properties_attributes" : { }
+    attrs = { "items" : [
+                {
+                  "href" : "http://localhost:8080/api/v1/clusters/cc/configurations/service_config_versions?service_name=HDFS&service_config_version=2",
+                  "configurations" : [
+                    {
+                      "config" : {
+                        "cluster_name" : "HDP",
+                        "stack_id" : "HDP-2.2"
+                      },
+                      "type" : "core-site",
+                      "tag" : "version2",
+                      "version" : 2,
+                      "properties" : {
+                            "security.inter.datanode.protocol.acl" : "*",
+                            "security.refresh.usertogroups.mappings.protocol.acl" : "hadoop",
+                            "security.client.datanode.protocol.acl" : "*",
+                            "security.admin.operations.protocol.acl" : "hadoop",
+                            "security.inter.tracker.protocol.acl" : "*",
+                            "security.datanode.protocol.acl" : "*",
+                            "security.job.client.protocol.acl" : "*",
+                            "security.client.protocol.acl" : "*",
+                            "security.job.task.protocol.acl" : "*",
+                            "security.refresh.policy.protocol.acl" : "hadoop",
+                            "security.namenode.protocol.acl" : "*"
+                      },
+                      "properties_attributes" : { }
+                    }
+                  ]
+                }
+            ]
         }
-      ]
-    }
-      ]
-    }
-    print(json.dumps(ambari_json))
-    # w = {"Person": {"name": "Tom", "age": 20, "childrens": [{"name": "Vasya"}, {"name": "Sergey"}]}}
-    # w = {"person": {"name": "Tom", "age": 20}, "padla": "dasyka", "dictkey": [{"inner_dict": "prikol"}]}
-    # d = [{"name": "Anton", "age": 20}, {"name": "Sergey", "age": 32}]
-    # d = {"name": "Anton", "age": 20}
-    # print(w)
-    # MyShinyClass = type('', (), w)()
-    # MyShinyClass.__setattr__({"name": "lala"})
-    # print(MyShinyClass.name)
+    l = {"person": {"name": "Tom", "age": 20}, "padla": "dasyka", "dictkey": [1, 2, 3, 4, 5]}
+    d = {"person": {"name": "Tom", "age": 20}, "padla": "dasyka", "dictkey": [{"krasava": "lala"}]}
 
-    # s = Base(w)
-    # print(s.dictkey.inner_dict)
+    l = BaseChild(l)
+    d = BaseChild(d)
+    winner = BaseChild(attrs)
+    # s = Base(attrs['items'][0]["configurations"][0])
+    print(winner.items[0].configurations[0].config.cluster_name)
+    # print(l.dictkey)
+    # print(d.dictkey[0].krasava)
